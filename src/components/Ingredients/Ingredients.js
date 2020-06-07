d35a7b5fc3fe10c0c80f23cf1ba1773874cdd2dd
Rohan Borakhatariya <rohan.borakhatariya@CDSYS.LOCAL>
2020-06-07 17:05:01 +0530

    Reducer is introduced instead of useState.

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,12 +1,26 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useReducer,useState, useEffect, useCallback } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
 import ErrorModal from "../UI/ErrorModal";
 import Search from "./Search";
 
+const ingredientReducer = (currentIngredients, action) => {
+  switch(action.type) {
+    case 'SET':
+      return action.ingredients
+    case 'ADD':
+      return [...currentIngredients, action.ingredient]
+    case 'DELETE':
+      return currentIngredients.filter(ing => ing.id !== action.id)
+    default:
+      throw new Error('Should not get there !')
+  }
+}
+
 function Ingredients() {
-  const [userIngredients, setUserIngredients] = useState([]);
+  const [userIngredients, dispatch] = useReducer(ingredientReducer, [])
+  // const [userIngredients, setUserIngredients] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
 
@@ -15,7 +29,8 @@ function Ingredients() {
   });
 
   const filteredIngredients = useCallback((filteredIngredients) => {
-    setUserIngredients(filteredIngredients);
+    // setUserIngredients(filteredIngredients);
+    dispatch({type: 'SET', ingredients: filteredIngredients})
   }, []); //caches function so it servive rerender cycle.
 
   const addIngredientHandler = (ingredient) => {
@@ -28,26 +43,28 @@ function Ingredients() {
       .then((response) => response.json())
       .then((responseData) => {
         setIsLoading(false);
-        setUserIngredients((prevIngredients) => [
-          ...prevIngredients,
-          { id: responseData.name, ...ingredient },
-        ]);
+        // setUserIngredients((prevIngredients) => [
+        //   ...prevIngredients,
+        //   { id: responseData.name, ...ingredient },
+        // ]);
+        dispatch({type: 'ADD', ingredient: {id: responseData.name, ...ingredient}})
       });
   };
 
   const removeIngredientHandler = (ingredientId) => {
     setIsLoading(true);
     fetch(
-      `https://react-hooks-update-f638c.firebaseio.com/ingredients/${ingredientId}.jon`,
+      `https://react-hooks-update-f638c.firebaseio.com/ingredients/${ingredientId}.json`,
       {
         method: "DELETE",
       }
     )
       .then((response) => {
         setIsLoading(false);
-        setUserIngredients((prevIngredients) =>
-          prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
-        );
+        // setUserIngredients((prevIngredients) =>
+        //   prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
+        // );
+        dispatch({type: 'DELETE', id: ingredientId})
       })
       .catch((error) => {
         setError("Something went wrong !");
